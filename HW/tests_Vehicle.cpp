1938bd12e6af169eeb19a4d82b6862ce4b75886a
mimssssi <maria.rogleva@gmail.com>
2021-04-16 14:36:19 +0300

    added class Vehicle, should test it

---
+++ HW/tests_Vehicle.cpp
@@ -0,0 +1,34 @@
+#include "catch2.hpp"
+#include "MyString.hpp"
+#include "Vehicle.hpp"
+
+TEST_CASE( "test Vehicle", "[MyString]" )
+{
+    const char registration[] = {"reg"};
+    const char description[] = {"descript"};
+    std::size_t space = 5;
+
+    Vehicle testVehicle(registration, description, space);
+    const char* reg = testVehicle.registration();
+    const char* descript = testVehicle.description();
+    std::size_t s = testVehicle.space();
+
+    SECTION("test registration")
+    {
+        REQUIRE(reg[0] == 'r');
+        REQUIRE(reg[1] == 'e');
+        REQUIRE(reg[2] == 'g');
+        REQUIRE(reg[3] == '\0');
+        REQUIRE_THROWS_AS(reg[4], std::out_of_range);
+    }
+    SECTION("test description")
+    {
+        REQUIRE(descript[2] == 's');
+        REQUIRE(descript[7] == '\0');
+        REQUIRE_THROWS_AS(descript[8], std::out_of_range);
+    }
+    SECTION("test space")
+    {
+        REQUIRE(s == 5);
+    }
+}
